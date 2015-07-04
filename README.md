102c2c204e3cb815c4b5da1ccccca0081489d668
root <root@localhost.localdomain>
2015-07-04 21:23:32 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# django14_tutorial
+
+centos 6.6
+python 2.6
+django 1.4
+
+の組み合わせで
+http://django-docs-ja.readthedocs.org/en/latest/intro/tutorial01.html
+のチュートリアルを実装してみました。
+
---
+++ polls/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Poll
+from django.views import generic
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list' : latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk = poll_id)
+    return render(request, 'polls/detail.html', {'poll' : poll})
+
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detial.html', {
+            'poll' : p,
+            'error_message' : "You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect('/polls/' + str(poll_id) + '/results')
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
